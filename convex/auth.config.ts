fc524a1db11939027302df372d012647b3d0f011
Pathak, Chinmay Rajendra <crpathak@iu.edu>
2024-06-05 08:32:02 -0400

    new-commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://grown-dane-91.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com'
+        },
+      ]
+    }
+  };
+  
+  export default nextConfig;
